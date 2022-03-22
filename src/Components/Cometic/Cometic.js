1b8e2af6e23cd8776b02dbaea5e7973371ba8ece
adir-jscode <adir.jscode@gmail.com>
2022-03-22 08:49:09 +0600

    array reduce

--- src/Components/Cometic/Cometic.js
+++ src/Components/Cometic/Cometic.js
@@ -1,20 +1,25 @@
 import React from 'react';
+import { getItem } from '../../utilities/calculate';
 import { addToDb, removeFromDb } from '../../utilities/localStorage';
 
 const Cometic = (props) => {
+    console.log(props);
     const { _id, name, age, gender, email } = props.cosmetic;
-    const addToCart = (id,age) => {
+    const addToCart = (id) => {
         addToDb(id);
     }
     const removeFromCart = (id) => {
         removeFromDb(id);
     }
 
+    // const totalAge = getItem(age);
+
     // const addToCartWithParam =() => addToCart(id)
     return (
         <div>
             <p>ID : { _id}</p>
             <h1>Name: {name}</h1>
+            {/* <h2>Age: {totalAge}</h2> */}
             <h3>Age : {age}</h3>
             <h3>Gender : {gender}</h3>
             <h3>Email : {email}</h3>
--- src/utilities/calculate.js
+++ src/utilities/calculate.js
@@ -6,4 +6,20 @@ const multiply = (first, second) => {
     return first * second;
 }
 
-export { add, multiply };
+// const items = [
+//     {id: 1, name: 'alta', price: 100},
+//     {id: 2, name: 'alta', price: 100},
+//     {id: 3, name: 'alta', price: 100},
+//     {id: 1, name: 'alta', price: 100},
+//     {id: 1, name: 'alta', price: 100},
+// ]
+// const item = items.reduce((previous, current) => previous + current.price, 0);
+// console.log(item);
+
+const getItem = (products) => {
+    const total = products.reduce((previous, current) => previous + current.price, 0);
+    return total;
+
+}
+
+export { add, multiply,getItem};
